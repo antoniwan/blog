c12d643fb1e385ea32cc5662cbda4d8720093357
Antonio Rodríguez Martínez <antoniwan@icloud.com>
2025-05-02 06:37:19 -0400

    Update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.stronghandssoftheart.com",
+  site: "https://blog.antoniwan.online",
   integrations: [mdx(), sitemap()],
   adapter: vercel(),
 });
