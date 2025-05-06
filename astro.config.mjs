c73fb482dcc0670044995e9dfb695376e9956d13
Antonio Rodriguez Martinez <antonio@builds.software>
2025-05-06 12:05:05 -0400

    Refactor site configuration by introducing constants for site URL, language flags, category emojis, and disclaimer text, enhancing maintainability and consistency across components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
+import { SITE_URL } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.antoniwan.online",
+  site: SITE_URL,
   integrations: [
     mdx(),
     sitemap(),
